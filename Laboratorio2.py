93e4fa83fcc5de353cea7b00194355e7d96f13a4
Protoss <mcquiam@ecc.cu>
2024-06-20 09:32:44 -0400

    Subiendo ejercicio1

--- Laboratorio2.py
+++ Laboratorio2.py
@@ -28,7 +28,6 @@ for col in DF_switzerland.columns:
 
 colum[colum.H & colum.C & colum.S]
 print(colum)
-
 select_colum = [
     'age',
     'sex',
@@ -45,3 +44,53 @@ select_colum = [
     'thal',
     'num'
 ]
+
+droping_colsC=[]
+for col in DF_cleveland.columns:
+    maxScore=0
+    for sel_col in select_colum:
+        if(similar(col,sel_col)>maxScore):
+            maxScore= similar(col,sel_col)
+    if(maxScore<0.7):
+        droping_colsC.append(col)
+    else:
+        if('previous' in col):
+            droping_colsC.append(col)
+
+DF_cleveland.drop(columns=droping_colsC,inplace=True)
+DF_cleveland.head(1)
+
+droping_colsH=[]
+for col in DF_hungarian.columns:
+    maxScore=0
+    for sel_col in select_colum:
+        if(similar(col,sel_col)>maxScore):
+            maxScore= similar(col,sel_col)
+    if(maxScore<0.7):
+        droping_colsH.append(col)
+    else:
+        if('previous' in col):
+            droping_colsH.append(col)
+
+DF_hungarian.drop(columns=droping_colsH,inplace=True)
+DF_hungarian.head(1)
+
+droping_colsS=[]
+for col in DF_switzerland.columns:
+    maxScore=0
+    for sel_col in select_colum:
+        if(similar(col,sel_col)>maxScore):
+            maxScore= similar(col,sel_col)
+    if(maxScore<0.7):
+        droping_colsS.append(col)
+    else:
+        if('previous' in col):
+            droping_colsS.append(col)
+
+DF_switzerland.drop(columns=droping_colsS,inplace=True)
+DF_switzerland.head(1)
+in_df= pd.concat([DF_cleveland,DF_hungarian,DF_switzerland])
+in_df.reset_index(inplace=True)
+in_df.drop(columns=['index'],inplace= True)
+in_df.head(1)
+
